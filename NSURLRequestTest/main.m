2021d435d3eb29ad6229d17fcd46487ba309fa06
YafeiBuaa <liyafeibuaa@163.com>
2014-04-28 19:54:19 +0800

    Initial Commit

---
+++ NSURLRequestTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSURLRequestTest
+//
+//  Created by apple on 14-4-28.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
